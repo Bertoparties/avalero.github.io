a4d7ef56eaa2c1949f8e953bd7c88bb20da25824
avalero <alberto.valero@bq.com>
2018-02-24 10:42:44 +0100

    install doku

---
+++ conf/users.auth.php
@@ -0,0 +1,12 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,seperated
+
+
+avalero:$1$zuMNst38$b374h1upZkn4dWVUQn3lM.:Alberto Valero:alberto.valero.gomez@gmail.com:admin,user
